66e5b2f4708ad7c18ec282b55acf4045b26697de
Lily <lolpo12345@outlook.com>
2022-04-08 05:03:41 +0300

    Add code to TodosController

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,48 @@
 class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save!
+      render json: @todo
+    else
+      redner json: @todo.errors.full_messages
+    end
+  end
+
+  def edit
+    @todo = Todo.new
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update!
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages
+    end
+  end
+
+  def delete
+    @todo = Todo.find(params[:id])
+
+    @todo.delete
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :status)
+  end
+
 end
